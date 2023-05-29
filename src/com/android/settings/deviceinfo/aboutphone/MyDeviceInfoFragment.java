YAAP-sweet/packages_apps_Settings-1

settings: Redesign about phone
Co-authored-by: mezz <mezzlasha@gmail.com>
Co-authored-by: imjyotiraditya <jyotiraditya@aospa.co>
Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>

src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -73,7 +73,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        use(ImeiInfoPreferenceController.class).setHost(this /* parent */);
         use(DeviceNamePreferenceController.class).setHost(this /* parent */);
         mBuildNumberPreferenceController = use(BuildNumberPreferenceController.class);
         mBuildNumberPreferenceController.setHost(this /* parent */);
@@ -82,7 +81,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
     @Override
     public void onStart() {
         super.onStart();
-        initHeader();
     }
 
     @Override
@@ -103,16 +101,13 @@ public class MyDeviceInfoFragment extends DashboardFragment
     private static List<AbstractPreferenceController> buildPreferenceControllers(
             Context context, MyDeviceInfoFragment fragment, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
-        controllers.add(new SimStatusPreferenceController(context, fragment));
-        controllers.add(new IpAddressPreferenceController(context, lifecycle));
-        controllers.add(new WifiMacAddressPreferenceController(context, lifecycle));
-        controllers.add(new BluetoothAddressPreferenceController(context, lifecycle));
         controllers.add(new RegulatoryInfoPreferenceController(context));
         controllers.add(new SafetyInfoPreferenceController(context));
         controllers.add(new ManualPreferenceController(context));
         controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new UptimePreferenceController(context, lifecycle));
+        controllers.add(new BlazeInfoPreferenceController(context));
         return controllers;
     }
 
@@ -122,40 +117,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
             return;
         }
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void initHeader() {
-        // TODO: Migrate into its own controller.
-        final LayoutPreference headerPreference =
-                getPreferenceScreen().findPreference(KEY_MY_DEVICE_INFO_HEADER);
-        final boolean shouldDisplayHeader = getContext().getResources().getBoolean(
-                R.bool.config_show_device_header_in_device_info);
-        headerPreference.setVisible(shouldDisplayHeader);
-        if (!shouldDisplayHeader) {
-            return;
-        }
-        final View headerView = headerPreference.findViewById(R.id.entity_header);
-        final Activity context = getActivity();
-        final Bundle bundle = getArguments();
-        final EntityHeaderController controller = EntityHeaderController
-                .newInstance(context, this, headerView)
-                .setRecyclerView(getListView(), getSettingsLifecycle())
-                .setButtonActions(EntityHeaderController.ActionType.ACTION_NONE,
-                        EntityHeaderController.ActionType.ACTION_NONE);
-
-        // TODO: There may be an avatar setting action we can use here.
-        final int iconId = bundle.getInt("icon_id", 0);
-        if (iconId == 0) {
-            final UserManager userManager = (UserManager) getActivity().getSystemService(
-                    Context.USER_SERVICE);
-            final UserInfo info = Utils.getExistingUser(userManager,
-                    android.os.Process.myUserHandle());
-            controller.setLabel(info.name);
-            controller.setIcon(
-                    com.android.settingslib.Utils.getUserIcon(getActivity(), userManager, info));
-        }
-
-        controller.done(context, true /* rebindActions */);
     }
 
     @Override

src/com/radiant/widget/WallpaperBlurView.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2021 Project Radiant
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.radiant.widget;
+
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+
+public class WallpaperBlurView extends ImageView {
+
+    Context contextM;
+
+    public WallpaperBlurView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
+    }
+
+    public WallpaperBlurView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
+    }
+
+
+    public WallpaperBlurView(Context context) {
+        super(context);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(contextM);
+        setImageDrawable(wallpaperManager.getDrawable());
+    }
+
+}

AOSPA-sweet/android_packages_apps_Settings

Change-Id: Ie571d45a7af7accee9ed7d26df471d6ba90b81f6

src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -100,7 +100,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
     @Override
     public void onStart() {
         super.onStart();
-        initHeader();
     }
 
     @Override
@@ -144,18 +143,13 @@ public class MyDeviceInfoFragment extends DashboardFragment
     private static List<AbstractPreferenceController> buildPreferenceControllers(
             Context context, MyDeviceInfoFragment fragment, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
-        controllers.add(new SimStatusPreferenceController(context, fragment));
-        controllers.add(new IpAddressPreferenceController(context, lifecycle));
-        controllers.add(new WifiMacAddressPreferenceController(context, lifecycle));
-        controllers.add(new BluetoothAddressPreferenceController(context, lifecycle));
         controllers.add(new RegulatoryInfoPreferenceController(context));
         controllers.add(new SafetyInfoPreferenceController(context));
         controllers.add(new ManualPreferenceController(context));
         controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new UptimePreferenceController(context, lifecycle));
-        controllers.add(new SoftwareVersionPreferenceController(context));
-        controllers.add(new StorageSizePreferenceController(context));
+        controllers.add(new AospaInfoPreferenceController(context));
         return controllers;
     }
 
@@ -165,40 +159,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
             return;
         }
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void initHeader() {
-        // TODO: Migrate into its own controller.
-        final LayoutPreference headerPreference =
-                getPreferenceScreen().findPreference(KEY_MY_DEVICE_INFO_HEADER);
-        final boolean shouldDisplayHeader = getContext().getResources().getBoolean(
-                R.bool.config_show_device_header_in_device_info);
-        headerPreference.setVisible(shouldDisplayHeader);
-        if (!shouldDisplayHeader) {
-            return;
-        }
-        final View headerView = headerPreference.findViewById(R.id.entity_header);
-        final Activity context = getActivity();
-        final Bundle bundle = getArguments();
-        final EntityHeaderController controller = EntityHeaderController
-                .newInstance(context, this, headerView)
-                .setRecyclerView(getListView(), getSettingsLifecycle())
-                .setButtonActions(EntityHeaderController.ActionType.ACTION_NONE,
-                        EntityHeaderController.ActionType.ACTION_NONE);
-
-        // TODO: There may be an avatar setting action we can use here.
-        final int iconId = bundle.getInt("icon_id", 0);
-        if (iconId == 0) {
-            final UserManager userManager = (UserManager) getActivity().getSystemService(
-                    Context.USER_SERVICE);
-            final UserInfo info = Utils.getExistingUser(userManager,
-                    android.os.Process.myUserHandle());
-            controller.setLabel(info.name);
-            controller.setIcon(
-                    com.android.settingslib.Utils.getUserIcon(getActivity(), userManager, info));
-        }
-
-        controller.done(context, true /* rebindActions */);
     }
 
     @Override

src/com/aospa/widget/WallpaperBlurView.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2021 Project Radiant
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aospa.widget;
+
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+
+public class WallpaperBlurView extends ImageView {
+
+    Context contextM;
+
+    public WallpaperBlurView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
+    }
+
+    public WallpaperBlurView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
+    }
+
+
+    public WallpaperBlurView(Context context) {
+        super(context);
+        contextM = context;
+        setRenderEffect(RenderEffect.createBlurEffect(15, 15, Shader.TileMode.CLAMP));
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(contextM);
+        setImageDrawable(wallpaperManager.getDrawable());
+    }
+
+}